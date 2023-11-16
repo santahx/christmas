ea5dd1bedaf3f7bd6c84ac0ac6af537d87fc0bdf
santa <santahydroix@gmail.com>
2023-11-16 05:55:52 +0100

    init

--- farming/init.lua
+++ farming/init.lua
@@ -22,8 +22,9 @@ dofile(farming.path .. "/hoes.lua")
 
 -- Wheat
 
+--Plant registration
 farming.register_plant("farming:wheat", {
-	description = S("Wheat Seed"),
+	description = S("Wheat Seed"), 
 	harvest_description = S("Wheat"),
 	paramtype2 = "meshoptions",
 	inventory_image = "farming_wheat_seed.png",
@@ -35,12 +36,14 @@ farming.register_plant("farming:wheat", {
 	place_param2 = 3,
 })
 
+--Register of item flour
 minetest.register_craftitem("farming:flour", {
 	description = S("Flour"),
 	inventory_image = "farming_flour.png",
 	groups = {food_flour = 1, flammable = 1},
 })
 
+--Register Item Bread
 minetest.register_craftitem("farming:bread", {
 	description = S("Bread"),
 	inventory_image = "farming_bread.png",
@@ -48,12 +51,14 @@ minetest.register_craftitem("farming:bread", {
 	groups = {food_bread = 1, flammable = 2},
 })
 
+--Register Craft recipe flour
 minetest.register_craft({
 	type = "shapeless",
 	output = "farming:flour",
 	recipe = {"farming:wheat", "farming:wheat", "farming:wheat", "farming:wheat"}
 })
 
+--Register baking craft recipe
 minetest.register_craft({
 	type = "cooking",
 	cooktime = 15,
@@ -64,6 +69,7 @@ minetest.register_craft({
 
 -- Hemp
 
+--Plant registration
 farming.register_plant("farming:hemp", {
 	description = S("Hemp Seed"),
 	harvest_description = S("Bud"),
@@ -77,12 +83,14 @@ farming.register_plant("farming:hemp", {
 	place_param2 = 3,
 })
 
+--Register of item Grinded Weed
 minetest.register_craftitem("farming:grinded_weed", {
 	description = S("Grinded Weed"),
 	inventory_image = "farming_grinded_bud.png",
 	groups = {food_flour = 1, flammable = 1},
 })
 
+--Register of item Joint
 minetest.register_craftitem("farming:joint", {
 	description = S("Joint"),
 	inventory_image = "farming_joint.png",
@@ -90,12 +98,14 @@ minetest.register_craftitem("farming:joint", {
 	groups = {food_bread = 1, flammable = 2},
 })
 
+--Register Craft recipe Grinded Weed
 minetest.register_craft({
 	type = "shapeless",
 	output = "farming:grinded_weed 3",
 	recipe = {"farming:hemp", "farming:hemp"}
 })
 
+--Register Craft recipe Joint
 minetest.register_craft({
 	type = "shapeless",
 	output = "farming:joint",
@@ -105,6 +115,7 @@ minetest.register_craft({
 
 -- Cotton
 
+--Plant registration
 farming.register_plant("farming:cotton", {
 	description = S("Cotton Seed"),
 	harvest_description = S("Cotton"),
@@ -116,11 +127,13 @@ farming.register_plant("farming:cotton", {
 	groups = {flammable = 4},
 })
 
+--Register Decoration 
 minetest.register_decoration({
 	name = "farming:cotton_wild",
 	deco_type = "simple",
 	place_on = {"default:dry_dirt_with_dry_grass"},
 	sidelen = 16,
+	--Platzierungs Parameter
 	noise_params = {
 		offset = -0.1,
 		scale = 0.1,
@@ -129,18 +142,22 @@ minetest.register_decoration({
 		octaves = 3,
 		persist = 0.7
 	},
+	-- Biome
 	biomes = {"savanna"},
 	y_max = 31000,
 	y_min = 1,
 	decoration = "farming:cotton_wild",
 })
 
+
+--Register Item string
 minetest.register_craftitem("farming:string", {
 	description = S("String"),
 	inventory_image = "farming_string.png",
 	groups = {flammable = 2},
 })
 
+--Register Craft Recipe whool
 minetest.register_craft({
 	output = "wool:white",
 	recipe = {
@@ -149,6 +166,7 @@ minetest.register_craft({
 	}
 })
 
+--Register Craft Recipe string
 minetest.register_craft({
 	output = "farming:string 2",
 	recipe = {
@@ -160,6 +178,7 @@ minetest.register_craft({
 
 -- Straw
 
+--Plant registration
 minetest.register_craft({
 	output = "farming:straw 3",
 	recipe = {
@@ -169,6 +188,7 @@ minetest.register_craft({
 	}
 })
 
+--Register Craft recipe Wheat
 minetest.register_craft({
 	output = "farming:wheat 3",
 	recipe = {
@@ -179,30 +199,35 @@ minetest.register_craft({
 
 -- Fuels
 
+--Wheat 
 minetest.register_craft({
 	type = "fuel",
 	recipe = "farming:wheat",
 	burntime = 1,
 })
 
+--Cotton
 minetest.register_craft({
 	type = "fuel",
 	recipe = "farming:cotton",
 	burntime = 1,
 })
 
+--String
 minetest.register_craft({
 	type = "fuel",
 	recipe = "farming:string",
 	burntime = 1,
 })
 
+--Hemp
 minetest.register_craft({
 	type = "fuel",
 	recipe = "farming:hemp",
 	burntime = 2,
 })
 
+--Wood
 minetest.register_craft({
 	type = "fuel",
 	recipe = "farming:hoe_wood",
@@ -222,20 +247,40 @@ if minetest.global_exists("dungeon_loot") then
 	})
 end
 
--- Funktion zur Überprüfung, ob ein Seil über der Pflanze ist
-local function check_rope_above(pos, nodename)
-    local above_pos = {x = pos.x, y = pos.y - 1, z = pos.z}
-    local above_node = minetest.get_node(above_pos)
 
-    minetest.log(above_node.name)
-    if above_node.name == "farming:seed_wheat" then
-     
+
+--Additional Grow size after placing fence over plant
+
+-- Funktion zur Überprüfung, ob ein Seil über der Pflanze ist
+local function check_rope_under(pos, nodename)
+    local under_pos = {x = pos.x, y = pos.y - 1, z = pos.z}
+    local under_node = minetest.get_node(under_pos)
+
+    --checks if a wheat seed is under it
+    if under_node.name == "farming:seed_wheat" then
+
+		--gives a log outpoot when detected
+    	minetest.log("detected")
     end
 end
 
 -- Registrieren einer on_placenode-Rückruffunktion für Pflanzenknoten
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
-	if (newnode.name == "default:fence_acacia_wood") then
-        check_rope_above(pos, newnode.name)
+
+	--default 
+	if (newnode.name == "default:fence_acacia_wood") 
+	or (newnode.name=="default:fence_aspen_wood")
+	or (newnode.name=="default:fence_junglewood") 
+	or (newnode.name=="default:fence_wood") 
+	or (newnode.name=="default:fence_pine_wood") 
+
+	--Everness support
+	or (newnode.name=="everness:fence_bamboo_wood") 
+	or (newnode.name=="everness:fence_crystal_wood") 
+	or (newnode.name=="everness:fence_dry_wood") 
+	or (newnode.name=="everness:fence__wood") 
+	then
+		--starts the check of node under it
+        check_rope_under(pos, newnode.name)
 	end
 end)
